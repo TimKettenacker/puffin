db6cd5a9f865aca5bc525208e867db120b02a34a
Tim Kettenacker <timkettenacker@Tims-MacBook-Pro.local>
2019-08-11 08:48:08 +0200

    added subject determination - work in progress

--- extract_pos_construct_triples.py
+++ extract_pos_construct_triples.py
@@ -21,6 +21,12 @@ def get_root(df):
         pass
     return root_predicate, root_position
 
+def get_mwe(df, word_position):
+    # determine multi word expressions
+    if df.iloc[word_position + 1,3] == "fixed" or "flat" or "compound":
+        mwe = df.iloc[word_position + 1, 1]
+    return mwe
+
 
 stanfordnlp.download('en')
 
@@ -29,62 +35,84 @@ doc = nlp(plot_dict[urls[22]])
 
 # data objects need to be extracted for ontologies, see https://stanfordnlp.github.io/stanfordnlp/data_objects.html
 # dependency_relation is a key element (word to grammar relationships); while 'nsubj', 'nsubj:pass', 'compound' and
-# 'flat' characterize subjects and 'obj' the object, relationship values are often indicated by the 'root' and sometimes
-# the 'conj' dependency. Yet, there is no exhaustive list of possible dependency values, but the alphabetical listing of
-# the Universal Dependency project is expanded on https://universaldependencies.org/u/dep/index.html
+# 'flat' characterize subjects and 'obj', 'obl' the object, relationship values are often indicated by the 'root' and
+# sometimes the 'conj' dependency. Yet, there is no exhaustive list of possible dependency values, but the alphabetical
+# listing of the Universal Dependency project is expanded on https://universaldependencies.org/u/dep/index.html
 # data object governor means a word that another word is dependent on
 
 # map data objects
 # try recognizing entities and map relations
 
-annotated_words = doc.sentences[0].words
+annotated_words = doc.sentences[1].words
 df = pandas.DataFrame()
 
 word_index = []
-word_lemma = []
+word_text = []
 word_upos = []
 word_dependency_relation = []
 word_governor = []
 
 for u in range(0, len(annotated_words)):
     word_index.append(annotated_words[u].index)
-    word_lemma.append(annotated_words[u].lemma)
+    word_text.append(annotated_words[u].text)
     word_upos.append(annotated_words[u].upos)
     word_dependency_relation.append(annotated_words[u].dependency_relation)
     word_governor.append(annotated_words[u].governor)
 
 df[0] = word_index
-df[1] = word_lemma
+df[1] = word_text
 df[2] = word_upos
 df[3] = word_dependency_relation
 df[4] = word_governor
 
 # clean up data frame
 df = df.loc[~df[2].str.contains('PUNCT')]
+df.columns = ['sentence_position', 'text', 'upos', 'dependency', 'governor_position']
 
 # extract the root of that very data frame
 root_elements = get_root(df)
 
 # extract objects
-objects_df = df.loc[df[3].str.contains('obj')]
+objects_df = df.loc[df[3].str.contains('obj|obl')]
 # if there is a reference in objects_df to the position of the root element, link it to the root
 # df.loc[df[4] == root_elements[1]]
 
 # extract subjects
 subjects = []
-subjects_df = df.loc[df[3].str.contains('nsubj|cc|conj|flat')]
-nsubj_position = int(subjects_df.loc[subjects_df[3].str.contains('nsubj')][0].values)
-nsubj_name = subjects_df.loc[subjects_df[3].str.contains('nsubj')][1].values[0]
-subjects.append(nsubj_name)
-# try to identify additional subjects
-if len(subjects_df) > 1:
-    try:
-        position_other_term = int(subjects_df.loc[subjects_df[4] == nsubj_position][0].values[0])
-        if ['CCONJ' in subjects_df[nsubj_position:position_other_term].values] == [True]:
-            second_subj_name = subjects_df.loc[subjects_df[4] == nsubj_position][1].values[0]
-            subjects.append(second_subj_name)
-    except:
-        pass
+# doing like it was done in next line will only create a copy of the dataframe
+# subjects_ref = df.loc[df[3].str.contains('subj|cc|conj|flat')]
+pos_subj = df.loc[df['dependency'].str.contains('subj')]['sentence_position'].values[0]
+name_subj = df.loc[df['dependency'].str.contains('subj')]['text'].values[0]
+# get mwes
+nxt_pos_dpndcy = df.loc[df.loc[:,'sentence_position'] == str(int(pos_subj)+1),'dependency']
+if nxt_pos_dpndcy.values[0] in ["fixed","flat","compound"]:
+    mwe = df.loc[df.loc[:,'sentence_position'] == str(int(pos_subj)+1),'text']
+    subject = name_subj + " " + mwe.values[0]
+    subjects.append(subject)
+if nxt_pos_dpndcy.values[0] not in ["fixed", "flat", "compound"]:
+    subjects.append(name_subj)
+# find additional subjects
+if nxt_pos_dpndcy.values[0] in ["cc"]:
+    upos_check = df.loc[df.loc[:, 'sentence_position'] == str(int(pos_subj) + 2), 'upos']
+    if upos_check.values[0] in ["PROPN","PRON"]:
+        second_subject = df.loc[df.loc[:, 'sentence_position'] == str(int(pos_subj) + 2), 'text']
+        subjects.append(second_subject.values[0])
 
-# to-do: add functionality that includes sur- last name, like Nick Fury, where last name is either flat or conj
-# do the same also if a second person is detected
+# to-do: make it a function, because the same is applicable to objects; extend functionality,
+# currently MWE + multiple subjects like "Nick Fury and Maria Hill" does not work correctly
+# compare output to open IE output
+
+# to-do: add functionality to fallback to any other word in case that i.e. nsubj is missing or no object is there
+
+
+from pycorenlp import StanfordCoreNLP
+inst = StanfordCoreNLP('http://localhost:9000')
+
+str_repr = ' '.join(word_text)
+
+output = inst.annotate(str_repr, properties={
+  'annotators': 'tokenize,ssplit,pos,depparse,parse,openie',
+  'outputFormat': 'json'
+  })
+
+print(output['sentences'][0]['openie'])
